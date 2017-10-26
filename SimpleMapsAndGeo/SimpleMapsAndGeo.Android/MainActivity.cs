b353a1716d99fd1184ea546be5efff027abb3b5c
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2017-10-27 01:55:05 +0800

    update mainactivity.cs

--- SimpleMapsAndGeo/SimpleMapsAndGeo.Android/MainActivity.cs
+++ SimpleMapsAndGeo/SimpleMapsAndGeo.Android/MainActivity.cs
@@ -20,7 +20,7 @@ namespace SimpleMapsAndGeo.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            Xamarin.FormsMaps.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
     }
